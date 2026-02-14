df69b759c8e6a562f5b8a8b29daf6354a1807b91
Simon Cropp <simon.cropp@gmail.com>
2026-02-14 11:08:01 +1100

    refs

--- src/Tests/ModuleInitializer.cs
+++ src/Tests/ModuleInitializer.cs
@@ -4,12 +4,15 @@ public static class ModuleInitializer
     public static void Init()
     {
         VerifierSettings.UseStrictJson();
-        VerifierSettings.InitializePlugins();
 
         // Force A4 size for consistent test results across regions
         DefaultPageSize.UseLetterSize = false;
 
         // Use 1.08 font width scale to better match Microsoft Word's text rendering
         DefaultFontSettings.FontWidthScale = 1.08;
+
+        VerifyImageMagick.RegisterComparers(threshold: 0.5);
+
+        VerifierSettings.InitializePlugins();
     }
 }
